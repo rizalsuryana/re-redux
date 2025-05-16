51e84c57fb606c9b67458bfbe1bbc575f2d1d5b5
rizalsuryana <rizal.suryn@gmail.com>
2025-05-17 05:30:48 +0700

    latihan lagi bikin schedule redux

--- redux-store/src/states/rootReducer.js
+++ redux-store/src/states/rootReducer.js
@@ -1,7 +1,9 @@
 import { goalReducer } from './goals/reducer';
+import { scheduleReducer } from './schedule/reducer';
 import { todosReducer } from './todos/reducer';
 
 export const rootReducer = (state = {}, action = {}) => ({
   todos: todosReducer(state.todos, action),
-  goals: goalReducer(state.goals, action)
+  goals: goalReducer(state.goals, action),
+  schedules: scheduleReducer(state.schedules, action)
 });
