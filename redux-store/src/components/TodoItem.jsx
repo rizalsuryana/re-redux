9605feba059dd2c40252c9c313fc727d3e9fb8b5
rizalsuryana <rizal.suryn@gmail.com>
2025-05-17 04:44:45 +0700

    add components : todoItem

---
+++ redux-store/src/components/TodoItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const TodoItem = ({
+  id, text, complete,
+  toggleTodo, deleteTodo
+}) => {
+  return (
+    <div>
+      <input type="checkbox" onChange={() => toggleTodo(id)} checked={complete}/>
+      <span>{text}</span>
+      <button onClick={() => deleteTodo(id)}>Delete Todo</button>
+    </div>
+  );
+};
