266d1ba0013db58abf92c2db155d8ce3b37adb49
rizalsuryana <rizal.suryn@gmail.com>
2025-05-25 09:22:08 +0700

    add share action

---
+++ open-space/src/states/shared/action.js
@@ -0,0 +1,30 @@
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
+import { usersAPI } from '../../api/userService';
+import { talksAPI } from '../../api/talkService';
+import { talksActionCreator } from '../talks/action';
+import { userActionCreator } from '../users/action';
+
+export const shareThunks = {
+  asyncPopulateUsersAndTalks: () => async (dispatch) => {
+    dispatch(showLoading());
+
+    try {
+    //   const users = await usersAPI.getAllUsers();
+    //   const talks = await talksAPI.getAllTalks();
+      const [users, talks] = await Promise.all([
+        usersAPI.getAllUsers(),
+        talksAPI.getAllTalks(),
+      ]);
+      if (!users || !talks){
+        throw new Error('Data users atau talks tidak valid');
+      }
+      dispatch(userActionCreator.receiveUsers(users));
+      dispatch(talksActionCreator.receiveTalks(talks));
+
+    } catch (error){
+      alert(error.message);
+    }
+
+    dispatch(hideLoading());
+  }
+};
