9ac48d044fc3ad86659ee3a795b3c510100dba32
rizalsuryana <rizal.suryn@gmail.com>
2025-05-26 06:18:14 +0700

    add register components

---
+++ open-space/src/components/RegisterInput.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useInput } from '../hooks/useInput';
+
+export const RegisterInput = ({ register }) => {
+  const [name, onNameChange] = useInput('');
+  const [id, onIdChange] = useInput('');
+  const [password, onPasswordChange] = useInput('');
+  return (
+    <form className='register-input'>
+      <input type='text' value={name} onChange={onNameChange} placeholder='Name'/>
+      <input type='text' value={id} onChange={onIdChange} placeholder='Username'/>
+      <input type='password' value={password} onChange={onPasswordChange} placeholder='Password'/>
+      <button type='button' onClick={() => register({ name, id, password })}>
+        Register
+      </button>
+    </form>
+  );
+};
+
+RegisterInput.propTypes = {
+  register: PropTypes.func.isRequired,
+};
